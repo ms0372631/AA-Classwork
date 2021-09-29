4dad1017db8ed9592f07365f7e60644bb9f82293
Alan Chiu <ms0372631@gmail.com>
2021-09-28 23:14:42 -0700

    deleted W1, W2 and W3

--- W1/D1 /advanced_methods_exercise/06_union.rb
+++
@@ -1,9 +0,0 @@
-# Write a method, union, that accepts any number of arrays as arguments.
-# The method should return an array containing all elements of the given arrays.
-
-def union(*array)
-    array.flatten
-end
-
-p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
-p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
--- W2/D3/battleship_project/lib/player.rb
+++
@@ -1,6 +0,0 @@
-class Player
-    def get_move
-        puts "enter a position with coordinates separeated with a space like '4 7'"
-        pos = [gets.chomp.split(" ")[0].to_i, gets.chomp.split(" ")[1].to_i]
-    end
-end

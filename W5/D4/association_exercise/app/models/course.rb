1a907b20384a95fa540bf9050a5639c9a963fb22
Alan Chiu <ms0372631@gmail.com>
2021-09-30 15:39:20 -0700

    done with enrolled_courses

---
+++ W5/D4/association_exercise/app/models/course.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+class Course < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+end
---
+++ W5/D4/association_exercise/app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    has_many :enrollments,
+        class_name: :Enrollment,
+        primary_key: :id,
+        foreign_key: :student_id
+
+    has_many :enrolled_courses,
+    through: :enrollments,
+    source: :courses
+
+end

e569f1bae4ef00d3ea3244ec0ccb9cde007ac34b
wesley <cheung.w.2016@gmail.com>
2021-09-30 17:03:18 -0700

    added prerequisite and instructior belongs_to

--- W5/D4/association_exercise/app/models/course.rb
+++ W5/D4/association_exercise/app/models/course.rb
@@ -19,4 +19,14 @@ class Course < ApplicationRecord
         through: :enrollments,
         source: :user
 
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true  
+    
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
 end
--- W5/D4/association_exercise/app/models/enrollment.rb
+++ W5/D4/association_exercise/app/models/enrollment.rb
@@ -18,4 +18,6 @@ class Enrollment < ApplicationRecord
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Course
+
+    
 end

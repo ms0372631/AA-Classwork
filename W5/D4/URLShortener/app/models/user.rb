ca252cd8dd3e64f82b34d8f00abfcb4f6265fb7e
Alan Chiu <ms0372631@gmail.com>
2021-09-30 17:46:34 -0700

    finished phase I

---
+++ W5/D4/URLShortener/app/models/user.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true 
+
+end

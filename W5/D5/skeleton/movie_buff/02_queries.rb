8c247840e98208ff5f8999c7327ef221c83de73b
Alan Chiu <ms0372631@gmail.com>
2021-10-03 05:30:53 -0700

    finished 02_queries

--- W5/D5/skeleton/movie_buff/02_queries.rb
+++ W5/D5/skeleton/movie_buff/02_queries.rb
@@ -21,10 +21,9 @@ def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
   Actor
-    .select(:id, :name)
-    .joins(:movie)
+    .joins(:movies)
     .where("movies.title = (?)", title)
-    .order(ord)
+    .order('castings.ord')
 
 end
 
@@ -32,7 +31,14 @@ def vanity_projects
   # List the title of all movies in which the director also appeared
   # as the starring actor.
   # Show the movie id and title and director's name.
-
+    
+    Movie
+      .select('movies.id, movies.title, actors.name')
+      .joins(:actors)
+      .where('director_id = actors.id')
+      .where('castings.ord = 1')
+  
+  
   # Note: Directors appear in the 'actors' table.
 
 end
@@ -40,5 +46,12 @@ end
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
+    Actor
+      .select('actors.id, actors.name, COUNT(*)')
+      .joins(:castings)
+      .where('castings.ord != 1')
+      .group('actors.id')
+      .order('COUNT(*)')
+      .limit(2)
 
 end

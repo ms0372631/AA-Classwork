27ec0ff3b0d5ce7f02050182b0ad9b9948c24253
Alan Chiu <69738597+ms0372631@users.noreply.github.com>
2021-10-07 03:28:03 -0700

    deleted file

--- W6/D3/my_project_name/app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def edit
-    # user = User.find(params[:id])
-    render json: params
-  end
-end
--- W6/D3/my_project_name/db/migrate/20211007000958_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
-    end
-  end
-end

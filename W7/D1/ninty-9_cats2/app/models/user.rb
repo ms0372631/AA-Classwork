86436ff023ab53099f72ac915dbf91c951737853
Alan Chiu <ms0372631@gmail.com>
2021-10-11 14:41:05 -0700

    finished user model

---
+++ W7/D1/ninty-9_cats2/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ApplicationRecord
+    after_initialize :ensure_session_token
+    validates :user_name, :password_digest, :session_token, presence: true
+    validates :session_token, uniqueness: true
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && 
+            BCrypt::Password.new(user.password_digest).is_password?(password)
+        nil
+    end
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+end

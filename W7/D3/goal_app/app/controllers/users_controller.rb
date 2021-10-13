7b0040b369fb5cd24202c2f90645d1d7d7562717
Jshin720 <jeffshin720@gmail.com>
2021-10-13 18:07:44 -0400

    finished the user controller and model

--- W7/D3/goal_app/app/controllers/users_controller.rb
+++ W7/D3/goal_app/app/controllers/users_controller.rb
@@ -1,2 +1,58 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def index 
+    @user = User.all
+    render :index
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @user = User
+  end
+
+  def show 
+    @user = User.find(param[:id])
+    render :show
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new, status: 422
+    end
+  end
+
+  def update 
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      redirect_to user_url 
+    else
+      render jsons: user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_url
+  end
+
+
+  def user_params 
+    params.require(:user).permit(:username)
+  end
+  
 end
--- W7/D3/goal_app/app/models/user.rb
+++ W7/D3/goal_app/app/models/user.rb
@@ -1,2 +1,40 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+  
+  validates :username, :email, session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.find_by_password?(password)
+      return user
+    else
+      nil
+    end
+  end
+
+  def is_valid_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
 end

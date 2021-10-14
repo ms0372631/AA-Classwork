a9710971dbc6435f8ff615b9f1ca4fa4543080cb
Alan Chiu <ms0372631@gmail.com>
2021-10-14 14:23:17 -0700

    header

--- W7/D4/aa_times/app/helpers/application_helper.rb
+++ W7/D4/aa_times/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def bottom_nav_list
+        list = ["World", "U.S.", "Politics", "N.Y.", "Business", "Opinion", "Tech", "Science", "Health", "Sports", "Arts", "Style", "Food", "Travel", "Magazine", "T Magazine", "Real Estate"]
+        str = ""
+        list.each do |ele|
+            str += "<li><a href='#'><span>#{ele}</span></a></li>"
+        end
+        str.html_safe
+    end
 end

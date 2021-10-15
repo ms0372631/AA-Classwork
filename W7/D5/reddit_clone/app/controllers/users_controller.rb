fc33acdc65f897cb61c0c9771f054056aed56be0
mjlomeli <mjlomeli@uci.edu>
2021-10-15 15:42:22 -0700

    added create user view

--- W7/D5/reddit_clone/app/controllers/users_controller.rb
+++ W7/D5/reddit_clone/app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
-  def create
-  end
+  before_action :require_logout, only: [:new, :create]
+
 
   def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to users_url
+    else
+      flash.now[:errrors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def show
   end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end

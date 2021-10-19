a29db9380eb57cd8bc7713759336951b70a88fec
tharpercp <tharpercp@gmail.com>
2021-10-18 17:03:12 -0700

    working on p02

--- W8/D1/rails_lite/bin/p01_basic_server.rb
+++ W8/D1/rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,24 @@
 require 'rack'
+require 'byebug'
+
+
+
+    # basic_app = Proc.new do |env|
+    #     req = Rack::Request.new(env)
+    #     res = Rack::Response.new
+    #     res['Content-Type'] = 'text/html'
+    #     res.write("Hello world!")
+    #     res.finish
+    # end
+
+    
+        basic_app = Proc.new do |env|
+          ['200', {'Content-Type' => 'text/html'}, ['hello world']]
+        end
+  
+
+
+  Rack::Server.start({
+    app: basic_app,
+    Port: 3000
+  })
--- W8/D1/rails_lite/bin/p02_controller_server.rb
+++ W8/D1/rails_lite/bin/p02_controller_server.rb
@@ -2,6 +2,29 @@ require 'rack'
 require_relative '../lib/controller_base'
 
 class MyController < ControllerBase
+
+  def self.call(env)
+    self.new(env).execute
+  end
+
+  attr_reader :req, :res
+
+  def initialize
+    @req = Rack::Request.new(env)
+    @res = Rack::Response.new
+  end
+
+  def render_content(content, content_type)
+    res.write(content)
+    set_header CONTENT_TYPE, content_type
+    nil
+  end
+
+  def redirect_to(url)
+    self.location = url
+  end
+
+
   def go
     if req.path == "/cats"
       render_content("hello cats!", "text/html")
@@ -10,15 +33,15 @@ class MyController < ControllerBase
     end
   end
 end
-app = Proc.new do |env|
-  req = Rack::Request.new(env)
-  res = Rack::Response.new
-  MyController.new(req, res).go
-  res.finish
-end
+# app = Proc.new do |env|
+#   req = Rack::Request.new(env)
+#   res = Rack::Response.new
+#   MyController.new(req, res).go
+#   res.finish
+# end
 
 Rack::Server.start(
-  app: app,
+  app: MyController,
   Port: 3000
 )
 

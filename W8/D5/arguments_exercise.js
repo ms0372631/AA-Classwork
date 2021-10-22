0662bfc71447cf7635075efaf097678f7b2758a2
MalachiCoberley <malachicoberley@gmail.com>
2021-10-22 14:53:10 -0700

    Complete Currying

--- W8/D5/arguments_exercise.js
+++ W8/D5/arguments_exercise.js
@@ -14,10 +14,28 @@ function sum() {
 
 console.log(sum(1, 2, 4));
 
-Function.prototype.myBind = function (obj) {
-    let myself = this;
-    return function {
-        myself.call(obj, ...args);
+Function.prototype.myBind = function (ctx, ...bindArgs) {
+    let func = this;
+    return function (...callArgs) {
+        let allArgs = bindArgs.concat(callArgs);
+        return func.apply(ctx, allArgs)
+        // myself.call(obj, ...args);
     }
 }
 
+function curriedSum(numArgs) {
+  let numbers = [];
+  return function _curriedSum(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      let sum = 0;
+      numbers.forEach(ele => sum += ele);
+      return sum;
+    } else {   
+      return _curriedSum;
+    }
+  };
+};
+
+const apples = curriedSum(4);
+apples(5)(30)(20)(1); // => 56
